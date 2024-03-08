aaf5d61de5708f127ea54f249b2ef9de688e1f3a
theanishtar <dangtt135@gmail.com>
2024-03-08 15:16:29 +0700

    init projects

---
+++ src/.vuepress/config.js
@@ -0,0 +1,96 @@
+const { description } = require('../../package')
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: 'Vuepress Docs Boilerplate',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: '',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    nav: [
+      {
+        text: 'Guide',
+        link: '/guide/',
+      },
+      {
+        text: 'Config',
+        link: '/config/'
+      },
+      {
+        text: 'VuePress',
+        link: 'https://v1.vuepress.vuejs.org'
+      }
+    ],
+    sidebar: {
+      '/guide/': [
+        {
+          title: 'Guide',
+          collapsable: false,
+          children: [
+            '',
+            'using-vue',
+          ]
+        }
+      ],
+    }
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    ["vuepress-plugin-cat"],
+
+    // [
+    //   'vuepress-plugin-helper-live2d', {
+    //     // 是否开启控制台日志打印(default: false)
+    //     log: false,
+    //     live2d: {
+    //       // 是否启用(关闭请设置为false)(default: true)
+    //       enable: true,
+    //       // 模型名称(default: hibiki)>>>取值请参考：
+    //       // https://github.com/JoeyBling/hexo-theme-yilia-plus/wiki/live2d%E6%A8%A1%E5%9E%8B%E5%8C%85%E5%B1%95%E7%A4%BA
+    //       model: 'hibiki',
+    //       display: {
+    //         position: "right", // 显示位置：left/right(default: 'right')
+    //         width: 135, // 模型的长度(default: 135)
+    //         height: 300, // 模型的高度(default: 300)
+    //         hOffset: 65, //  水平偏移(default: 65)
+    //         vOffset: 0, //  垂直偏移(default: 0)
+    //       },
+    //       mobile: {
+    //         show: false // 是否在移动设备上显示(default: false)
+    //       },
+    //       react: {
+    //         opacity: 0.8 // 模型透明度(default: 0.8)
+    //       }
+    //     }
+    //   }
+    // ]
+  ]
+}
