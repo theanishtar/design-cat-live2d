822e902ea3f89e6958cdf44ebd61eda23e55386e
theanishtar <dangtt135@gmail.com>
2024-03-12 14:33:25 +0700

    ci: config package.json to build

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -64,6 +64,8 @@ module.exports = {
    * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
    */
   plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
     ["vuepress-plugin-cat"],
 
     // [
